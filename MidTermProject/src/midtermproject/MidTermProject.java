99665f1600878958edd62a8c17b2ac1876d6ab75
Jonathan <Jonathananders6@gmail.com>
2018-09-27 21:18:32 -0400

    You need to study tableView

--- MidTermProject/src/midtermproject/MidTermProject.java
+++ MidTermProject/src/midtermproject/MidTermProject.java
@@ -2,24 +2,37 @@ package midtermproject;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Statement;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
 
 /**
  *
  * @author Jonathan Anders
  */
 public class MidTermProject extends Application {
+    
+    private ObservableList<ObservableList> data;
     
     private BorderPane main = new BorderPane();
     private Statement queryStatement;
@@ -36,6 +49,7 @@ public class MidTermProject extends Application {
         
         //Connect to DB
         initializeDataBase();
+         displayResults();
     }
 
     //Returns the top menubar and all the controls
@@ -72,7 +86,7 @@ public class MidTermProject extends Application {
         orderTotal.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent t)
             {
-               
+               main.setCenter(getOrderDetailsDesign());
             }
         }); 
         
@@ -80,7 +94,7 @@ public class MidTermProject extends Application {
         orderDetail.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent t)
             {
-               
+               main.setCenter(getOrderDetailsDesign());
             }
         }); 
         
@@ -97,7 +111,7 @@ public class MidTermProject extends Application {
         customerDetails.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent t)
             {
-               
+               main.setCenter(getCustomerDetailBox());
             }
         });
         
@@ -115,13 +129,118 @@ public class MidTermProject extends Application {
         employeeBirthdays.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent t)
             {
-               
+               main.setCenter(getEmployeeDetailsBox());
             }
         }); 
         
         menuEmployee.getItems().addAll(employeeBirthdays);
         
         return menuEmployee;
+    }
+    
+    //Returns hbox for all input for getting order details
+    private HBox getOrderDetailsDesign()
+    {
+        HBox orderTotalBox = new HBox();
+        
+        Label orderNumLabel = new Label("Enter Order Number");
+        TextField orderNumberField = new TextField();
+        Button submitQueryButton = new Button("Submit");
+        submitQueryButton.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t)
+            {
+
+            }
+        });         
+        
+        orderTotalBox.getChildren().addAll(orderNumLabel, orderNumberField, submitQueryButton);
+        orderTotalBox.setSpacing(5);
+        orderTotalBox.setPadding(new Insets(5));
+        
+        return orderTotalBox;
+    }
+    
+    private HBox getCustomerDetailBox()
+    {
+        HBox customerDetailBox = new HBox();
+        
+        Label stateCodeLabel = new Label("Enter State Code");
+        TextField stateCodeInputBox = new TextField();
+        Button submitQueryButton = new Button("Submit");
+        submitQueryButton.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t)
+            {
+
+            }
+        }); 
+        
+        customerDetailBox.getChildren().addAll(stateCodeLabel, stateCodeInputBox, submitQueryButton);
+        customerDetailBox.setSpacing(5);
+        customerDetailBox.setPadding(new Insets(5));
+        
+        return customerDetailBox;
+    }
+
+    private HBox getEmployeeDetailsBox()
+    {
+        HBox EmployeeDetailBox = new HBox();
+        
+        Label yearLabel = new Label("Enter Year");
+        TextField yearInputBox = new TextField();
+        Button submitQueryButton = new Button("Submit");
+        submitQueryButton.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t)
+            {
+
+            }
+        }); 
+        
+        EmployeeDetailBox.getChildren().addAll(yearLabel, yearInputBox, submitQueryButton);
+        EmployeeDetailBox.setSpacing(5);
+        EmployeeDetailBox.setPadding(new Insets(5));
+        
+        return EmployeeDetailBox;
+    }
+    
+    private void displayResults()
+    {
+        TableView resultsTable = new TableView();
+        data = FXCollections.observableArrayList();
+        
+        try
+        {
+            String queryString = "select * from Employees";
+          
+            ResultSet resultSet = queryStatement.executeQuery(queryString);
+            ResultSetMetaData rsMetaData = resultSet.getMetaData();
+            
+            for (int i = 1; i <= rsMetaData.getColumnCount(); i++)
+            {
+                TableColumn test = new TableColumn(rsMetaData.getColumnName(i));
+                System.out.println(rsMetaData.getColumnName(i) + "    ");
+                test.setResizable(true);
+                resultsTable.getColumns().addAll(test);
+            }
+            // Iterate through the result and print the student names
+            while (resultSet.next()) 
+            {
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rsMetaData.getColumnCount(); i++)
+                {
+                    row.add(resultSet.getString(i));
+                }
+                data.add(row);
+            }
+            
+            resultsTable.setItems(data);
+        }
+        catch(SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+       
+         
+        main.setCenter(resultsTable);
     }
     
     //Connect to the database
